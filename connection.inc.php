710fef6073d3eb9b48e40598f9a4be5fabbc7765
mahmuda-bjit <mahmuda.keya@bjitacademy.com>
2023-06-12 20:15:29 +0000

    adding script files

--- connection.inc.php
+++ connection.inc.php
@@ -10,15 +10,8 @@ $con=mysqli_connect("ecom.cuil8gwatjrr.ap-northeast-3.rds.amazonaws.com", "admin
 define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/');
 
 //Edit the IP address (127.0.0.1) with the Public IP address of your EC2 instance
-// Determine the protocol based on the request
-$protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://';
+define('SITE_PATH','http://website-lb-210271409.ap-northeast-3.elb.amazonaws.com/');
 
-// Edit the domain name accordingly
-$domain = 'website-lb-210271409.ap-northeast-3.elb.amazonaws.com';
-
-define('SITE_PATH', $protocol . $domain . '/');
-
-define('PRODUCT_IMAGE_SERVER_PATH', SERVER_PATH . 'media/product/');
-define('PRODUCT_IMAGE_SITE_PATH', SITE_PATH . 'media/product/');
+define('PRODUCT_IMAGE_SERVER_PATH',SERVER_PATH.'media/product/');
+define('PRODUCT_IMAGE_SITE_PATH',SITE_PATH.'media/product/');
 ?>
-
